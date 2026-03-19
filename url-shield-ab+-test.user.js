842e9ba76e9d3bdfc6e86833aedd908d36f70dc6
and1 <131588191+ancandi@users.noreply.github.com>
2026-03-20 00:13:56 +0800

    Update url-shield-ab+-test.user.js

--- url-shield-ab+-test.user.js
+++ url-shield-ab+-test.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name YouTube Mobile URL Shield AB+ Ultra-Short
-// @version 6.5.9
+// @name YouTube Mobile URL Shield AB+ Short/Optimized
+// @version 3.0.3
 // @match https://*.youtube.com/*
 // @run-at document-start
 // ==/UserScript==
